1e527a273361d42d9eba23b7dd8eaf6d6b508aa5
AbhiAadi <113759846+AbhiAadi@users.noreply.github.com>
2023-07-17 09:53:05 +0530

    Update chatgui.py

--- chatgui.py
+++ chatgui.py
@@ -8,7 +8,7 @@ from tensorflow.keras.models import load_model
 model = load_model('chatbot_model.h5')
 import json
 import random
-intents = json.loads(open('data/chatbot dataset/intents.json').read())
+intents = json.loads(open('data/intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
 
